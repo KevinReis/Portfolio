e988f163654c65b50ad18ec5d6f39ce11674d7e4
Kevin Reis Barbosa <kevinreis1@gmail.com>
2022-10-19 17:27:13 -0300

    Alterações

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing", {
-    strings:["","Desenvolvedor Web", "Estudante de Direito", "Ex-Jogador","Júnior Back-End"],
+    strings:["","Desenvolvedor Web", "Estudante de Direito", "Ex-Jogador de futebol de base","Júnior Back-End"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
